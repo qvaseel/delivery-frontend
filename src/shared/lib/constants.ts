2f311d6763a45d9a7d1610978e970e0dc8961c50
qvaseel <vasilina.makarkina@gmail.com>
2026-05-02 20:36:54 +0700

    add docker

--- src/shared/lib/constants.ts
+++ src/shared/lib/constants.ts
@@ -1,1 +1,4 @@
+// const rawApiUrl = import.meta.env.VITE_API_URL?.trim() ?? "";
+
+// export const API_URL = rawApiUrl.replace(/\/+$/, "");
 export const API_URL = "https://localhost:7212"; // твой бэк
