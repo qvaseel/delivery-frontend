89649547632872ec83a3f333b8b7d4518cc55e67
qvaseel <vasilina.makarkina@gmail.com>
2026-05-02 22:12:00 +0700

    fix const

--- src/shared/lib/constants.ts
+++ src/shared/lib/constants.ts
@@ -1,4 +1,4 @@
-// const rawApiUrl = import.meta.env.VITE_API_URL?.trim() ?? "";
+const rawApiUrl = import.meta.env.VITE_API_URL?.trim() ?? "";
 
-// export const API_URL = rawApiUrl.replace(/\/+$/, "");
-export const API_URL = "https://localhost:7212"; // твой бэк
+export const API_URL = rawApiUrl.replace(/\/+$/, "");
+// export const API_URL = "https://localhost:7212"; // твой бэк
